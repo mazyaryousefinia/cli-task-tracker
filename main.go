515b1a4515f1b0eac7d9b25b17e2d15160e43d65
mazyaryousefinia <mazyaryousefinia@gmail.com>
2024-10-13 23:36:38 +0330

    feat: add task struct

--- main.go
+++ main.go
@@ -5,7 +5,24 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
+
+type Status string
+
+const (
+	StatusTodo       Status = "todo"
+	StatusInProgress Status = "in-progress"
+	StatusDone       Status = "done"
+)
+
+type Task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      Status    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
 
 func main() {
 
